9eead84b7b1d98e8c9fea4f44ee9066c21c80597
Henry Jones <hj084021@ohio.edu>
2022-09-07 17:45:48 -0400

    I wrote a program!

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: henry jones
+ *        Date: 9/7/2022
+ * Description: isk
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "HelloGit!!" << endl;
+    return 0;
+}// main 
